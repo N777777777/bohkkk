212a41729d42d7ec2ae2767460e876c1f28599d3
N777777777 <nttnaa905@gmail.com>
2025-11-02 15:56:07 +0200

    Create core-system-v2.js

---
+++ core-system-v2.js
@@ -0,0 +1,19 @@
+// System Core Configuration v2.1.8
+// Protected Module - Do Not Modify
+const firebaseConfig = {
+  apiKey: "AIzaSyBVagvNXYSADrSoALrysaWkTjLfZZ-wtJ0",
+  authDomain: "mmadnan9k-63525.firebaseapp.com",
+  databaseURL: "https://mmadnan9k-63525-default-rtdb.firebaseio.com",
+  projectId: "mmadnan9k-63525",
+  storageBucket: "mmadnan9k-63525.firebasestorage.app",
+  messagingSenderId: "336186568291",
+  appId: "1:336186568291:web:ad71f1cf2fe1baf6501256",
+  measurementId: "G-5PZBK5VZ4Q"
+};
+
+// Promotional System Config
+const promoCodes = {
+  "NEWTON1": 0.001,
+  "NEWTON2": 50,
+  "NEWTON3": 25
+};
